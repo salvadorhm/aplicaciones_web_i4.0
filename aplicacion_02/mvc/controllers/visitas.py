e4d00be47aebf183378031e1f3b4300130fd1b39
Salvador Hernández Mendoza <salvadorhm@gmail.com>
2020-05-23 23:39:43 +0000

    Cookies como parametro y contador de visitas

--- aplicacion_02/mvc/controllers/visitas.py
+++ aplicacion_02/mvc/controllers/visitas.py
@@ -1,17 +1,25 @@
 import web
 
 class Visitas:
-    def GET(self, name):
+    def GET(self, nombre):
         try:
             cookie = web.cookies()
+            visitas = "0"
             print(cookie)
+            if nombre:
+                web.setcookie("nombre",nombre,expires="", domain=None)
+            else:
+                nombre = "NA"
+                web.setcookie("nombre",nombre,expires="", domain=None)
+
             if cookie.get("visitas"):
                 visitas = int(cookie.get("visitas"))
                 visitas += 1
                 web.setcookie("visitas", str(visitas), expires="", domain=None)
-                return "Visitas " + str(visitas)
             else:
                 web.setcookie("visitas", str(1), expires="", domain=None)
-                return "Visitas " + "1"
+                visitas = "1"
+            
+            return "Visitas " + str(visitas) + " Nombre " + nombre
         except Exception as e:
             return "Error " + str(e.args)
